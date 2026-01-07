05f6c7267a20aa91d58a07f73541a94c87a3af9e
duser <user@email.fr>
2026-01-07 16:49:30 +0100

    Ajout de fonction login et logout

--- fonctions.php
+++ fonctions.php
@@ -22,6 +22,7 @@ function connDb()
         echo '<h1>Erreur de connexion PDO !!!</h1> ' . $erreur->getMessage() . '<br>';
         die('Impossible de se connecter à la base de donnée !');
     }
+    return $pdo;
 }
 
 function dg($data)
@@ -44,4 +45,22 @@ function redirect($url)
 {
     require $url;
     die();
+}
+
+function is_logged_in() {
+    return isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true;
+}
+
+function logout_user() {
+    $_SESSION = [];
+    session_destroy();
+    session_unset();
+}
+
+function login_user() {
+
+}
+
+function register_user() {
+    
 }
