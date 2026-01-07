e5ad055b2b96fcb0e04fcedc3abc4a25dd3abe97
duser <user@email.fr>
2026-01-07 16:30:23 +0100

    Php Blog

---
+++ fonctions.php
@@ -0,0 +1,47 @@
+<?php
+function connDb()
+{
+    $host    = $configdb['host'];
+    $db      = $configdb['db'];
+    $user    = $configdb['user'];
+    $pass    = $configdb['pass'];
+    $charset = $configdb['charset'];
+
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+    $options = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+
+    try {
+        $pdo = new PDO($dsn, $user, $pass, $options);
+    } catch (PDOException $erreur) {
+        echo '<h1>Erreur de connexion PDO !!!</h1> ' . $erreur->getMessage() . '<br>';
+        die('Impossible de se connecter à la base de donnée !');
+    }
+}
+
+function dg($data)
+{
+    echo '<pre style="background-color:black; color:white;padding: 1rem;">';
+    var_dump($data);
+    echo '</pre>';
+}
+
+
+function dd($data)
+{
+    echo '<pre style="background-color:black; color:white;padding: 1rem;">';
+    var_dump($data);
+    echo '</pre>';
+    die();
+}
+
+function redirect($url)
+{
+    require $url;
+    die();
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+# PHP Mini Blog Systeme
+
